89527818562abbb5fef8dd7b8e3d1a18884d90cb
root <root@localhost.localdomain>
2016-06-10 15:11:03 -0400

    third

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var request = require('supertest');
 var app = require('../app.js');
 describe('GET /', function() {
  it('respond with hello world', function(done){
-  request(app).get('/').expect('hello world', done);
+  request(app).get('/').expect('Hello World', done);
   });
 });
